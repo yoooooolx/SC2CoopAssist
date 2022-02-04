64c23ab96dfa1333672a1abbc7e1106b38faecd3
Mr.Facetious <mr.facetious@qq.com>
2022-02-04 16:15:09 +0800

    MainActivity completed

---
+++ app/src/main/java/com/facetious/mr/sc2coopassist/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.facetious.mr.sc2coopassist
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlin.system.exitProcess
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun startAssist(view: View) {
+        val intent = Intent(this, activityStart::class.java)
+        startActivity(intent)
+    }
+
+    fun assistOptions(view: View) {
+        val intent = Intent(this, activityOptions::class.java)
+        startActivity(intent)
+    }
+
+    fun exitAssist(view: View) {
+        exitProcess(0)
+    }
+}
---
+++ app/src/main/java/com/facetious/mr/sc2coopassist/activityOptions.kt
@@ -0,0 +1,11 @@
+package com.facetious.mr.sc2coopassist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activityOptions : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_options)
+    }
+}
