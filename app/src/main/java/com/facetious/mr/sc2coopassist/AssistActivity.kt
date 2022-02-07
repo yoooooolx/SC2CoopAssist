409e1c0377c293866a68699a0a35ff7db161c8fe
Mr.Facetious <mr.facetious@qq.com>
2022-02-07 17:18:14 +0800

    Map select

--- app/src/main/java/com/facetious/mr/sc2coopassist/AssistActivity.kt
+++ app/src/main/java/com/facetious/mr/sc2coopassist/AssistActivity.kt
@@ -7,5 +7,17 @@ class AssistActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_assist)
+        val mapname = intent.getIntExtra(MAP_NAME,0)
+        when (mapname)
+        {
+            1-> //克哈裂痕
+            {
+                //
+            }
+            2-> //虚空撕裂
+            {
+                //
+            }
+        }
     }
 }
